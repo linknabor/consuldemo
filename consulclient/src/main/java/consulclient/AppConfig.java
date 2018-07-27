f5edd579137f6754e9cf1f282dff86270f4e2637
davidhardson <davidhardson@hotmail.com>
2018-07-27 16:27:33 +0800

    consul demo

---
+++ consulclient/src/main/java/consulclient/AppConfig.java
@@ -0,0 +1,25 @@
+package consulclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Configuration
+@EnableDiscoveryClient
+@RestController
+public class AppConfig {
+
+	@RequestMapping("/hello")
+	public String home() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConfig.class, args);
+	}
+
+}
